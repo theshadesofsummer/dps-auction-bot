88a5980d0cd52e6699c3265fa369752c02addde1
theshadesofsummer <louisschulze03@gmail.com>
2023-07-07 21:21:15 +0200

    updated logs

--- src/abi-interaction.js
+++ src/abi-interaction.js
@@ -6,7 +6,7 @@ module.exports = {
 }
 
 const ABI_DPS_AUCTION = require('./abi/ABI_DPS_AUCTION.json');
-const {handleBid} = require("./auction-interaction");
+const {handleBid, handleAuctionSettling} = require("./auction-interaction");
 const CONTRACT_ADDRESS_DPS_AUCTION = '0xc9B1eF7FF1BFBAF35f68C8650045F350e1CEee65';
 const DPS_AUCTION = new web3.eth.Contract(ABI_DPS_AUCTION, CONTRACT_ADDRESS_DPS_AUCTION);
 
@@ -24,13 +24,10 @@ function setupAuctionListener() {
 
   DPS_AUCTION.events.AuctionSettled(() => {
   }).on("connected", function(_subscriptionId){
-    console.log('connected to contract!');
+    console.log('connected to auction settling!');
   })
     .on('data', function(event){
-      console.log('Event:', event);
-      console.log('Owner Wallet Address: ',event.returnValues.owner);
-
-      handleBid(event)
+      handleAuctionSettling(event)
     })
     .on('error', function(error, receipt) {
       console.log('Error:', error, receipt);
