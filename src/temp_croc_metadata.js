e9c081ca43f32650f5802052b042006bb90ced26
theshadesofsummer <louisschulze03@gmail.com>
2023-11-29 23:21:06 +0100

    update pirate to 8625

--- src/temp_croc_metadata.js
+++ src/temp_croc_metadata.js
@@ -1,84 +1,84 @@
 module.exports = {
-  "external_url": "https://www.damnedpiratessociety.io/gen1pirates/8624",
-  "image": "https://dps-gen1-meta.s3.amazonaws.com/8624.png",
-  "name": "Occhiali",
+  "external_url": "https://www.damnedpiratessociety.io/gen1pirates/8625",
+  "image": "https://dps-gen1-meta.s3.amazonaws.com/8625.png",
+  "name": "Robert Edward Moor",
   "description": "",
   "attributes": [
-  {
-    "trait_type": "Background",
-    "value": "GDS"
-  },
-  {
-    "trait_type": "Body",
-    "value": "Pink"
-  },
-  {
-    "trait_type": "Peg Leg",
-    "value": "None"
-  },
-  {
-    "trait_type": "Boots",
-    "value": "None"
-  },
-  {
-    "trait_type": "Belt",
-    "value": "Buckle Compass"
-  },
-  {
-    "trait_type": "Uniform",
-    "value": "Ceremonial Green"
-  },
-  {
-    "trait_type": "Hat",
-    "value": "Shark Teeth"
-  },
-  {
-    "trait_type": "Mouth Type",
-    "value": "Open"
-  },
-  {
-    "trait_type": "Blood",
-    "value": "None"
-  },
-  {
-    "trait_type": "Mouth Accessory",
-    "value": "None"
-  },
-  {
-    "trait_type": "Injured",
-    "value": "None"
-  },
-  {
-    "trait_type": "Eye type",
-    "value": "Bloodshot"
-  },
-  {
-    "trait_type": "Eye Special",
-    "value": "None"
-  },
-  {
-    "trait_type": "Eye Patch",
-    "value": "Eyepatch Emerald"
-  },
-  {
-    "trait_type": "Wrist",
-    "value": "None"
-  },
-  {
-    "trait_type": "Tattoo",
-    "value": "None"
-  },
-  {
-    "trait_type": "Luck",
-    "value": "280"
-  },
-  {
-    "trait_type": "Navigation",
-    "value": "280"
-  },
-  {
-    "trait_type": "Strength",
-    "value": "280"
-  }
-]
+    {
+      "trait_type": "Background",
+      "value": "GDN"
+    },
+    {
+      "trait_type": "Body",
+      "value": "Red"
+    },
+    {
+      "trait_type": "Peg Leg",
+      "value": "None"
+    },
+    {
+      "trait_type": "Boots",
+      "value": "None"
+    },
+    {
+      "trait_type": "Belt",
+      "value": "None"
+    },
+    {
+      "trait_type": "Uniform",
+      "value": "Guildmaster"
+    },
+    {
+      "trait_type": "Hat",
+      "value": "Guildmaster"
+    },
+    {
+      "trait_type": "Mouth Type",
+      "value": "Closed"
+    },
+    {
+      "trait_type": "Blood",
+      "value": "None"
+    },
+    {
+      "trait_type": "Mouth Accessory",
+      "value": "Dagger"
+    },
+    {
+      "trait_type": "Injured",
+      "value": "None"
+    },
+    {
+      "trait_type": "Eye type",
+      "value": "Bloodshot"
+    },
+    {
+      "trait_type": "Eye Special",
+      "value": "None"
+    },
+    {
+      "trait_type": "Eye Patch",
+      "value": "Eyepatch Gold Coin"
+    },
+    {
+      "trait_type": "Wrist",
+      "value": "Wrist Coin"
+    },
+    {
+      "trait_type": "Tattoo",
+      "value": "Tattoo Doubloon"
+    },
+    {
+      "trait_type": "Luck",
+      "value": "270"
+    },
+    {
+      "trait_type": "Navigation",
+      "value": "270"
+    },
+    {
+      "trait_type": "Strength",
+      "value": "270"
+    }
+  ]
 }
