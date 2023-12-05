9e567db4ad57bc5ecd144a5680af6f3feb4751ea
theshadesofsummer <louisschulze03@gmail.com>
2023-12-05 09:26:22 +0100

    update pirate to 8626

--- src/temp_croc_metadata.js
+++ src/temp_croc_metadata.js
@@ -1,84 +1,84 @@
-module.exports = {
-  "external_url": "https://www.damnedpiratessociety.io/gen1pirates/8625",
-  "image": "https://dps-gen1-meta.s3.amazonaws.com/8625.png",
-  "name": "Robert Edward Moor",
+{
+  "external_url": "https://www.damnedpiratessociety.io/gen1pirates/8626",
+  "image": "https://dps-gen1-meta.s3.amazonaws.com/8626.png",
+  "name": "El Blanco",
   "description": "",
   "attributes": [
-    {
-      "trait_type": "Background",
-      "value": "GDN"
-    },
-    {
-      "trait_type": "Body",
-      "value": "Red"
-    },
-    {
-      "trait_type": "Peg Leg",
-      "value": "None"
-    },
-    {
-      "trait_type": "Boots",
-      "value": "None"
-    },
-    {
-      "trait_type": "Belt",
-      "value": "None"
-    },
-    {
-      "trait_type": "Uniform",
-      "value": "Guildmaster"
-    },
-    {
-      "trait_type": "Hat",
-      "value": "Guildmaster"
-    },
-    {
-      "trait_type": "Mouth Type",
-      "value": "Closed"
-    },
-    {
-      "trait_type": "Blood",
-      "value": "None"
-    },
-    {
-      "trait_type": "Mouth Accessory",
-      "value": "Dagger"
-    },
-    {
-      "trait_type": "Injured",
-      "value": "None"
-    },
-    {
-      "trait_type": "Eye type",
-      "value": "Bloodshot"
-    },
-    {
-      "trait_type": "Eye Special",
-      "value": "None"
-    },
-    {
-      "trait_type": "Eye Patch",
-      "value": "Eyepatch Gold Coin"
-    },
-    {
-      "trait_type": "Wrist",
-      "value": "Wrist Coin"
-    },
-    {
-      "trait_type": "Tattoo",
-      "value": "Tattoo Doubloon"
-    },
-    {
-      "trait_type": "Luck",
-      "value": "270"
-    },
-    {
-      "trait_type": "Navigation",
-      "value": "270"
-    },
-    {
-      "trait_type": "Strength",
-      "value": "270"
-    }
-  ]
+  {
+    "trait_type": "Background",
+    "value": "TD"
+  },
+  {
+    "trait_type": "Body",
+    "value": "White"
+  },
+  {
+    "trait_type": "Peg Leg",
+    "value": "None"
+  },
+  {
+    "trait_type": "Boots",
+    "value": "None"
+  },
+  {
+    "trait_type": "Belt",
+    "value": "None"
+  },
+  {
+    "trait_type": "Uniform",
+    "value": "None"
+  },
+  {
+    "trait_type": "Hat",
+    "value": "Noner"
+  },
+  {
+    "trait_type": "Mouth Type",
+    "value": "Open"
+  },
+  {
+    "trait_type": "Blood",
+    "value": "None"
+  },
+  {
+    "trait_type": "Mouth Accessory",
+    "value": "None"
+  },
+  {
+    "trait_type": "Injured",
+    "value": "None"
+  },
+  {
+    "trait_type": "Eye type",
+    "value": "Blind"
+  },
+  {
+    "trait_type": "Eye Special",
+    "value": "None"
+  },
+  {
+    "trait_type": "Eye Patch",
+    "value": "None"
+  },
+  {
+    "trait_type": "Wrist",
+    "value": "None"
+  },
+  {
+    "trait_type": "Tattoo",
+    "value": "None"
+  },
+  {
+    "trait_type": "Luck",
+    "value": "300"
+  },
+  {
+    "trait_type": "Navigation",
+    "value": "300"
+  },
+  {
+    "trait_type": "Strength",
+    "value": "300"
+  }
+]
 }
